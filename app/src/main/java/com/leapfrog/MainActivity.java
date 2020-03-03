c5c5b5119aaef61e9de40c3d35420b90786149ac
Omri Chashper <oc76@drexel.edu>
2020-03-03 18:12:22 -0500

    Input Section w/out Gallery and Camer Access

--- app/src/main/java/com/leapfrog/MainActivity.java
+++ app/src/main/java/com/leapfrog/MainActivity.java
@@ -1,27 +1,25 @@
 package com.leapfrog;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.leapfrog.activities.ChatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.leapfrog.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button button = findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
