fc34b5567c8c87efcb20a5702c47757d3a16a957
sra84 <saseer@stuy.edu>
2020-03-05 18:18:06 -0500

    Working send message

--- app/src/main/java/com/leapfrog/activities/ChatActivity.java
+++ app/src/main/java/com/leapfrog/activities/ChatActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -13,7 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.CustomTarget;
@@ -22,16 +23,34 @@ import com.leapfrog.DisplayMessageActivity;
 import com.leapfrog.R;
 
 import java.lang.invoke.ConstantCallSite;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class ChatActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    public ArrayList<String> myDataset= new ArrayList<>();
+
     public static final String EXTRA_MESSAGE = "com.leapfrog.MESSAGE";
     ImageButton sendButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
         userName = " " + userName;
@@ -40,11 +59,10 @@ public class ChatActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ChatActivity.this, DisplayMessageActivity.class);
                 EditText editText = (EditText) findViewById(R.id.editText);
                 String message = editText.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
+                myDataset.add(message);
+                mAdapter.notifyDataSetChanged();
             }
         });
 
