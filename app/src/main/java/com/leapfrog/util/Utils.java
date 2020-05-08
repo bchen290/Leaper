e1823929b2342111d7e50cb6aa529a1feb94e8f1
Brian Chen <brianchen0810@hotmail.com>
2020-05-08 13:16:48 -0400

    Add network check

--- app/src/main/java/com/leapfrog/util/Utils.java
+++ app/src/main/java/com/leapfrog/util/Utils.java
@@ -1,5 +1,13 @@
 package com.leapfrog.util;
 
-public class Utils {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
+public class Utils {
+    public boolean hasNetworkAvailable(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
+    }
 }
