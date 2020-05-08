2d31e1cfd2d2555137357731f828d03fd3e330e0
Brian Chen <brianchen0810@hotmail.com>
2020-05-08 19:32:45 -0400

    Make register button bring user to RegistrationActivity

--- app/src/main/java/com/leapfrog/activities/LoginActivity.java
+++ app/src/main/java/com/leapfrog/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.leapfrog.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,15 +19,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         final TextView userName = findViewById(R.id.Username);
-        final EditText passWord = findViewById(R.id.Password);
+        final EditText password = findViewById(R.id.Password);
+
+        final TextView registerLink = findViewById(R.id.lnkRegister);
+        registerLink.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegistrationActivity.class);
+            startActivity(intent);
+        });
 
         Button login = findViewById(R.id.btnLogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean real=LeaperDatabase.getInstance(LoginActivity.this).verifyData(userName.getText().toString(),passWord.getText().toString());
-
-            }
+        login.setOnClickListener(v -> {
+            boolean validUser = LeaperDatabase.getInstance(LoginActivity.this).verifyData(userName.getText().toString(), password.getText().toString());
         });
     }
 }
