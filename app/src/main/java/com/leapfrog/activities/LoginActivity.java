11f10dc7e7ded33cbfdca1a77aaa115232f57827
sra84 <saseer@stuy.edu>
2020-05-14 17:02:14 -0400

    Login popup if wrong

--- app/src/main/java/com/leapfrog/activities/LoginActivity.java
+++ app/src/main/java/com/leapfrog/activities/LoginActivity.java
@@ -2,7 +2,6 @@ package com.leapfrog.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +12,7 @@ import com.leapfrog.database.LeaperDatabase;
 import com.leapfrogandroid.R;
 
 public class LoginActivity extends AppCompatActivity {
+    boolean logged =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +20,27 @@ public class LoginActivity extends AppCompatActivity {
 
         final TextView userName = findViewById(R.id.Username);
         final EditText password = findViewById(R.id.Password);
-
+        final TextView wrong = findViewById(R.id.wrong);
         final TextView registerLink = findViewById(R.id.lnkRegister);
         registerLink.setOnClickListener(v -> {
             Intent intent = new Intent(this, RegistrationActivity.class);
             startActivity(intent);
         });
 
+
         Button login = findViewById(R.id.btnLogin);
         login.setOnClickListener(v -> {
             boolean validUser = LeaperDatabase.getInstance(LoginActivity.this).verifyData(userName.getText().toString(), password.getText().toString());
+            if(validUser==false){
+                wrong.setText("Wrong Username or Password");
+                wrong.setTextSize(25);
+            }
+            else{
+                logged =true;
+            }
         });
+    }
+    public boolean isLoggedin(){
+        return logged;
     }
 }
