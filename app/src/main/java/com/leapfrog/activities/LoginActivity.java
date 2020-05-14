83ed4be2a960e0ff3072d2668dac262e3ca1c906
Brian Chen <brianchen0810@hotmail.com>
2020-05-14 16:34:30 -0400

    Authenticate user after login button is pressed

--- app/src/main/java/com/leapfrog/activities/LoginActivity.java
+++ app/src/main/java/com/leapfrog/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.leapfrog.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,13 @@ public class LoginActivity extends AppCompatActivity {
         Button login = findViewById(R.id.btnLogin);
         login.setOnClickListener(v -> {
             boolean validUser = LeaperDatabase.getInstance(LoginActivity.this).verifyData(userName.getText().toString(), password.getText().toString());
+
+            if(validUser) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Authentication", MODE_PRIVATE);
+                sharedPreferences.edit()
+                        .putBoolean("IsAuthenticated", true)
+                        .apply();
+            }
         });
     }
 }
