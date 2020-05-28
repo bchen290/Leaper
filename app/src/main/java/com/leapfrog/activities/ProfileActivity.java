5c2bd0f3886419c8e4b8e081548220e9fba2836e
Brian Chen <brianchen0810@hotmail.com>
2020-05-28 19:40:25 -0400

    Allow user to select profile picture from gallery

--- app/src/main/java/com/leapfrog/activities/ProfileActivity.java
+++ app/src/main/java/com/leapfrog/activities/ProfileActivity.java
@@ -1,58 +1,36 @@
 package com.leapfrog.activities;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.leapfrogandroid.R;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import com.bumptech.glide.Glide;
+import com.esafirm.imagepicker.features.ImagePicker;
+import com.esafirm.imagepicker.model.Image;
+import com.leapfrogandroid.R;
+
 public class ProfileActivity extends AppCompatActivity {
-    private static final int SELECT_PICTURE = 0;
     private CircleImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_screen);
-        imageView = (CircleImageView) findViewById(R.id.profile_image);
+        imageView = findViewById(R.id.profile_image);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Bitmap bitmap = getPath(data.getData());
-            imageView.setImageBitmap(bitmap);
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            Image image = ImagePicker.getFirstImageOrNull(data);
+            Glide.with(this).load(image.getPath()).into(imageView);
         }
-    }
-
-    private Bitmap getPath(Uri uri) {
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String filePath = cursor.getString(column_index);
-        // cursor.close();
-        // Convert file path into bitmap image using below line.
-        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-
-        return bitmap;
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void selectImage(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+        ImagePicker.create(this).single().start();
     }
 }
