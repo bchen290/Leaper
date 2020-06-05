3e432d43f9de617b767325c5485b5bdda9769c76
ged42 <ged42@drexel.edu>
2020-06-05 13:42:18 -0400

    InterenetConnectivty and Authentication documentation

--- app/src/main/java/com/leapfrog/util/Authentication.java
+++ app/src/main/java/com/leapfrog/util/Authentication.java
@@ -5,19 +5,42 @@ import android.content.SharedPreferences;
 
 import static android.content.Context.MODE_PRIVATE;
 
+/**
+ * This class does the authentication for the app
+ */
 public class Authentication {
+    /**
+     * Gets the preferences for authentication
+     * @param context context for getting authentication preference
+     * @return returns the preference for authentication
+     */
     private static SharedPreferences getAuthenticationPreference(Context context) {
         return context.getSharedPreferences("Authentication", MODE_PRIVATE);
     }
 
+    /**
+     * Checks if the context is authenticated
+     * @param context the context to be authenticated
+     * @return returns true if it is authenticated, false if not
+     */
     public static boolean isAuthenticated(Context context) {
         return getAuthenticationPreference(context).getBoolean("IsAuthenticated", false);
     }
 
+    /**
+     * Retrives the username of the user
+     * @param context context for username
+     * @return returns the username
+     */
     public static String getUsername(Context context) {
         return getAuthenticationPreference(context).getString("Username", "");
     }
 
+    /**
+     * Authenticates the username for the context
+     * @param context context of the user
+     * @param username the username
+     */
     public static void authenticate(Context context, String username) {
         getAuthenticationPreference(context).edit()
                 .putBoolean("IsAuthenticated", true)
@@ -25,6 +48,10 @@ public class Authentication {
                 .apply();
     }
 
+    /**
+     * Unauthenticates the user
+     * @param context context of user
+     */
     public static void unauthenticate(Context context) {
         getAuthenticationPreference(context).edit()
                 .putBoolean("IsAuthenticated", false)
--- app/src/main/java/com/leapfrog/util/InternetConnectivity.java
+++ app/src/main/java/com/leapfrog/util/InternetConnectivity.java
@@ -5,15 +5,33 @@ import android.content.SharedPreferences;
 
 import static android.content.Context.MODE_PRIVATE;
 
+/**
+ * Checks the internet connectivity
+ */
 public class InternetConnectivity {
+    /**
+     * Gets the internet connectivity preference
+     * @param context context for preference
+     * @return returns the internet connectivity preference
+     */
     private static SharedPreferences getInternetConnectivityPreference(Context context) {
         return context.getSharedPreferences("InternetConnectivity", MODE_PRIVATE);
     }
 
+    /**
+     * Caches the current internet state
+     * @param context context to be cached
+     * @param hasInternet boolean for if user has internet connectivity
+     */
     public static void cacheInternetState(Context context, boolean hasInternet) {
         getInternetConnectivityPreference(context).edit().putBoolean("HasInternet", hasInternet).apply();
     }
 
+    /**
+     * checks the cached internet  preference
+     * @param context context to get the cached internet
+     * @return returns the cached internet preferences
+     */
     public static boolean checkCachedInternet(Context context) {
         return getInternetConnectivityPreference(context).getBoolean("HasInternet", false);
     }
